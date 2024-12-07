877752efb87ec5ec5c712591109aaf67f3879fde
dd-jiyun <dlwldbs8015@gmail.com>
2024-12-07 19:24:53 +0900

    first commit

---
+++ src/test/java/aws/aws_test/AwsTestApplicationTests.java
@@ -0,0 +1,13 @@
+package aws.aws_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
