bdaba679ce6b9d514b116cc320835a6ae0a6c38d
dd-jiyun <dlwldbs8015@gmail.com>
2024-12-07 19:49:09 +0900

    refactor controller

--- src/main/java/aws/aws_test/HelloController.java
+++ src/main/java/aws/aws_test/HelloController.java
@@ -1,12 +1,12 @@
 package aws.aws_test;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @GetMapping("hello")
+    @GetMapping("/hello")
     public String Hello() {
         return "hello";
     }
