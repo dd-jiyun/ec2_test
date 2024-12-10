4628bda95194d2494d2be8b17207471272154eef
dd-jiyun <dlwldbs8015@gmail.com>
2024-12-10 21:24:09 +0900

    docker environment test

--- src/main/java/aws/aws_test/HelloController.java
+++ src/main/java/aws/aws_test/HelloController.java
@@ -1,14 +1,26 @@
 package aws.aws_test;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    private final String testMsg;
+
+    public HelloController(@Value("${msg}") String testMsg) {
+        this.testMsg = testMsg;
+    }
+
     @GetMapping("/hello")
     public String Hello() {
         return "hello";
+    }
+
+    @GetMapping("/hi")
+    public String hi() {
+        return testMsg;
     }
 
 }
